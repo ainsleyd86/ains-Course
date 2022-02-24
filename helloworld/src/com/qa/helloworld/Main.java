5df889479a01fd28b4fde23e3a4eebe554d6b950
ainsleyd86 <ainsley.dobbs@outlook.com>
2022-02-24 01:18:35 +0000

    adding .java files

---
+++ helloworld/src/com/qa/helloworld/Main.java
@@ -0,0 +1,9 @@
+package com.qa.helloworld;
+//to use a class from a dif package we need to import it
+public class Main {
+//like a container for related functions
+// (sections of a supermarket, veg,home etc)
+    public static void main(String[] args) {
+//line needed to execute code
+    }
+}
---
+++ helloworld/src/com/qa/loops/Loop.java
@@ -0,0 +1,89 @@
+package com.qa.loops;
+
+public class Loop {
+
+    public static void main(String[] args) {
+
+//oops) allow us to repeat a block of code until a
+// specified condition is met.
+//Each loop will check for a condition and as soon as
+// that condition is broken, the code will stop
+// running that loop. There are three main loop types
+// in Java:
+
+//while()-loop - use w/BOOL like IF statement that dont
+//stop executing until the condition is no longer met.
+
+//do()-while()-loop -
+
+//for()-loop
+       /*WHILE loop example ----
+        int notEnoughMoney = 1;
+        boolean justEnoughMoney = true;
+
+        while(justEnoughMoney) {
+            System.out.println("More P's");
+
+            notEnoughMoney++;
+
+            if(notEnoughMoney > ) {
+                justEnoughMoney = false;
+            }
+        }
+
+        System.out.println("lets get those creps");
+    }
+}*/
+
+       /* do while loop example
+    int playCount = 0;
+    boolean playing = true;
+
+
+            do {
+                System.out.println("Playing");
+                playCount++;
+
+                if(playCount > 10) {
+                    playing = false;
+
+//In the main method we a running a do()-while()-loop
+// that prints "Playing", then adds 1 to the variable playCount,
+// then runs the conditional checking if playCount is
+// greater than 10.
+//If playCount is greater than 10, then we go into the
+// if()-statement and set playing to false.
+
+                }
+            } while(playing);
+
+            System.out.println("Game Over!");
+        }
+    }*/
+
+  /*  FOR LOOP
+*In the  example we have three statements
+within the for()-loop.
+
+*The first one - int i = 0; -
+is initialising the counter for the
+iteration as an integer with the name i and the value 0.
+
+*Best practice for the name of your counter variable
+is to use i.
+
+*The second statement i < 10; is our condition, and
+tells Java to run the code within the for()-loop
+until i is greater than or equal to 10.
+
+*The third statement i++ is simply telling Java to
+increment our counter, in this case i, by 1 after it
+executes the last line of code within our for loop.
+
+*so this example we end up printing "Hello There" 10x
+*/
+
+ //       for(int i = 0; i < 10; i++) {
+   //         System.out.println("Hello There!");
+        }
+    }
